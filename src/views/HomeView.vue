0aa17807819dec9ea639852520ed4849e1701083
panutat.p <pp.panutat@gmail.com>
2023-12-17 01:43:45 +0700

    feat: use Pinia store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,10 @@ const counter = useCounterStore()
 
 <template>
   <h1 class="text-4xl mb-10">home</h1>
+  <button v-on:click="counter.increment" class="btn mb-10">
+    Increment
+    <div class="badge badge-secondary">{{ counter.count }}</div>
+  </button>
   <SquareSpinner class="mb-10" />
   <img src="https://placehold.co/768x50/857979/FFF" alt="simple placeholder" />
   <img src="https://placehold.co/1024x50/696362/FFF" alt="simple placeholder" />
