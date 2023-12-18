be6f2376ef784ae1304d632bfdf5945110c21ea1
panutat.p <pp.panutat@gmail.com>
2023-12-18 22:13:50 +0700

    feat: weird store

--- src/views/Part2.vue
+++ src/views/Part2.vue
@@ -1,13 +1,36 @@
-<script setup lang="ts">
-import { useBasicStore } from '@/stores/basic'
+<script lang="ts">
+import { computed, ref } from 'vue'
 
-const { double, n } = useBasicStore()
+let store: any
+
+const n = ref(0)
+
+const double = computed(() => n.value * 2)
+
+function increment() {
+  n.value++
+}
+
+store = { n, double, increment }
+
+/**
+ * useWeirdStore is OK because:
+ * 1. No need to init in the App.vue
+ * 2. JS GC will not destroy this store.
+ */
+export function useWeirdStore() {
+  return store
+}
+</script>
+
+<script setup lang="ts">
+const { n, double, increment } = useWeirdStore()
 </script>
 
 <template>
   <h1 class="text-4xl mb-10">Part 2</h1>
-  <h2 class="text-2xl mb-10">Global Store</h2>
-  <button v-on:click="n++" class="btn btn-primary mb-5">Increment</button>
+  <h2 class="text-2xl mb-10">Weird Store</h2>
+  <button v-on:click="increment" class="btn btn-primary mb-5">Increment</button>
   <p class="text-2xl mb-5">{{ n }}</p>
   <p class="text-2xl mb-5">{{ double }}</p>
 </template>
